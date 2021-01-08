3af87b31a295922b452e39ebe97df55858ba51b2
kosminskimateusz <kosminskimateusz@gmail.com>
2021-01-08 19:20:36 +0100

    Added class User.cs, Added lists

---
+++ Class/User.cs
@@ -0,0 +1,9 @@
+namespace LinqPractice
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqPractice
 {
@@ -6,7 +8,56 @@ namespace LinqPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var listOfInts = new List<int>
+            {
+                3,5,4,7,22,8,5,1
+            };
+
+            var listOfStrings = new List<string>
+            {
+                "Ala",
+                "Ma",
+                "Dużego",
+                "Fiuta",
+                "",
+                "Kośmiński"
+            }
+
+
+            var listOfUsers = new List<User>
+            {
+                new User
+                {
+                    FirstName = "Papryk",
+                    LastName = "Korzeń",
+                    Email = "paprykkorzen@gmail.com"
+                },
+
+                new User
+                {
+                    FirstName = "Michał",
+                    LastName = "Korzenny",
+                    Email = "michalkorzenny@gmail.com"
+                },
+                new User
+                {
+                    FirstName = "Mateusz",
+                    Email = "mateuszoko@gmail.com"
+                },
+                null
+            };
+
+            var list1 = listOfInts.Select(x => x + 2).ToList();                  // skrót
+            
+            listOfInts.Select(x => {return x + 2;});        // robi to samo co wyżej
+
+
+            // Func<int, int> power = (a) => { return a * a; };
+            // Action functionLambda2 = () => { Console.WriteLine("Hi"); };
+
+            // functionLambda2();
+            // functionLambda2.Invoke();       // to samo co wywołanie powyżej
+            // Console.WriteLine(power(6));
         }
     }
 }
